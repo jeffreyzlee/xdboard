204d7bbad2d62f47615d8d347e3b5e0cd932de3b
Erick Wilson <stealthbomber600@gmail.com>
2018-11-14 23:37:48 -0800

    got db working for adding food

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
 
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+var docClient = new AWS.DynamoDB.DocumentClient();
+
 
 var weight = 135;
 var target = 145;
@@ -12,6 +15,7 @@ var height = 175;
 var foodsAdded = [];
 
 app.get('/home', (req, res) => {
+	console.log(foodsAdded);
 	res.sendFile(__dirname + '/home.html');
 });
 
@@ -64,7 +68,56 @@ app.get('/getThings', (req, res) => {
 	}
 
 	res.json(toSend);
-})
+});
+
+app.post('/addFood', (req, res) => {
+	var params = {
+        TableName: "Foods",
+        Item: req.body
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add movie. Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:");
+       }
+    });
+});
+
+app.get('/foodToAdd', (req, res) => {
+	var toSend = {
+		foodsAdded: [],
+	};
+
+	var params = {
+	    TableName: "Foods",
+	};
+
+	docClient.scan(params, (err, data) => {
+		if (err) {
+        	console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        	res.json(toSend);
+    	} else {
+        	// print all the movies
+        	console.log(toSend.foodsAdded);
+        	var i = 0;
+        	for(i = 0; i < data.Items.length; i++){
+        		toSend.foodsAdded.push(data.Items[i]);
+        	}
+
+        	res.json(toSend);
+    	}
+	});
+
+
+
+	
+});
+
+app.get('/help', (req, res) => {
+	res.sendFile(__dirname + '/help.html');
+});
 
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 module.exports = app;
